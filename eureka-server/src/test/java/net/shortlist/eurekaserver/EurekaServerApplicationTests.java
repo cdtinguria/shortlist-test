984a8d1eab1629c24d598548aba44a9b5a344dd1
Chetan Tinguria <chetan.tinguria@gmail.com>
2019-12-18 07:57:51 +0530

    adding eureka server

---
+++ eureka-server/src/test/java/net/shortlist/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shortlist.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
